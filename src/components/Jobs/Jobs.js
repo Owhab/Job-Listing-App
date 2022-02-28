d91ded304d9a610d5a0a3d853da2b601004dc04f
Abdul Owhab <owhab.cse@gmail.com>
2022-03-01 02:52:51 +0600

    Added Filtering Functionality

--- src/components/Jobs/Jobs.js
+++ src/components/Jobs/Jobs.js
@@ -5,13 +5,13 @@ import Job from "../Job/Job";
 
 const Jobs = () => {
   const [jobs, setJobs] = useState([]);
-  const [filters, setFilters] = useState(["CSS"]);
+  const [filters, setFilters] = useState([]);
 
   useEffect(() => {
     setJobs(jobsdata);
   }, []);
 
-  const filterFunc = (role, level, tools, languages) => {
+  const filterFunc = ({ role, level, tools, languages }) => {
     if (filters.length === 0) {
       return true;
     }
@@ -27,6 +27,7 @@ const Jobs = () => {
     return tags.some((tag) => filters.includes(tag));
   };
 
+  // Avoid Re-adding the tag to
   const handleTagClick = (tag) => {
     if (filters.includes(tag)) return;
 
@@ -40,19 +41,22 @@ const Jobs = () => {
 
   return (
     <div>
-      <div className="sm:flex-row sm:my-8 flex flex-col mx-10 my-16  shadow-md rounded-md bg-white m-4 p-4">
-        {filters.length > 0 &&
-          filters.map((filter, index) => (
+      {filters.length > 0 && (
+        // Remove the filtering component from header when filter is empty
+        <div className="sm:flex-row sm:my-8 flex flex-col mx-10 my-16  shadow-md rounded-md bg-white m-4 p-4">
+          {filters.map((filter, index) => (
             <span
               className="bg-teal-100 cursor-pointer font-bold text-teal-500 my-2 mr-4 p-2 rounded"
               onClick={() => handleFilterClick(filter)}
               key={index}
             >
-              {filter}
+              {filter} ✕
             </span>
           ))}
-      </div>
-      {jobs.map((job) => (
+        </div>
+      )}
+
+      {filteredJobs.map((job) => (
         <Job key={job.id} job={job} handleTagClick={handleTagClick}></Job>
       ))}
     </div>
